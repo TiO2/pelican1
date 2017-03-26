84831d4469f95a246df81a45ff08024254dd47ed
pei-macPro <pei-macpro@pei-macPros-MBP.home>
2017-03-25 23:37:40 -0400

    test Flex theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,3 +33,9 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Theme
+#THEME = "/Users/pei-macpro/Desktop/pelican-themes/Flex"
+THEME = "theme"
+
+
